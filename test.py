b550e23170ab3ba42a0e96847ec5fa35c2391153
agtor <agtoro17@gmail.com>
2026-07-14 12:44:22 +0200

    Remove Euler's constant

--- test.py
+++ test.py
@@ -37,11 +37,4 @@ for obj in objs:
         except ValueError:
             print('pow: impossible')
 
-print("===")
-print(pn(3) // pn(2))
-print(pn(0.1)+pn(0.2))
-print(math.floor(pn(3.4)))
-print(math.ceil(pn(3.4)))
-
-print(math.pi, pn.PI)
-print(pn(math.pi)-pn.PI)
+print(pn.PI + 1)
